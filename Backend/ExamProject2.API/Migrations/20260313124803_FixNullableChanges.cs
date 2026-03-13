8b096da94fcf3f7e8d2b5b0daab2022d5c10536d
ranjitwn <rannai02711@stud.noroff.no>
2026-03-13 13:49:28 +0100

    Apply migration for nullable model changes

---
+++ Backend/ExamProject2.API/Migrations/20260313124803_FixNullableChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamProject2.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNullableChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
